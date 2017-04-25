5f8907c1a4d7d35e8c88ba16acdd5836d935355a
rhand7 <handje@BC-MacBookPro-0587d1.local>
2017-04-25 12:28:37 -0600

    Add CloudKitManager Functionality

--- BulletinBoard2/BulletinBoard2/CloudKitManager.swift
+++ BulletinBoard2/BulletinBoard2/CloudKitManager.swift
@@ -11,6 +11,23 @@ import CloudKit
 
 class CloudKitManager {
     
-    let database = CKContainer.default().publicCloudDatabase 
+    let database = CKContainer.default().publicCloudDatabase
+    
+    // Fetch
+                                            // = nil allows this code to be reusable either sort or not
+    func fetchRecords(ofType type: String, sortDescriptors: [NSSortDescriptor]? = nil, completion: @escaping ([CKRecord]?, Error?) -> Void) {
+        let query = CKQuery(recordType: type, predicate: NSPredicate(value: true))
+        query.sortDescriptors = sortDescriptors    //equal to itself so we can reuse code w/ other types in our controller
+        
+        database.perform(query, inZoneWith: nil, completionHandler: completion) //calls all queries
+    }
+    
+    // Save
+    
+    func save(_ record: CKRecord, completion: @escaping (Error?) -> Void) {
+        self.database.save(record) { (record, error) in
+            completion(error)
+        }
+    }
     
 }
